4de9e5ec9c09e87a6bb92343df024a8493e949b8
chakra2017 <bhattacharjee.chakra17@gmail.com>
2023-01-07 12:31:26 +0530

    Add files via upload

---
+++ src/routes/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from '../components/Header';
+import HeroSection from '../components/HeroSection';
+import Specials from '../components/Specials';
+import Footer from '../components/Footer';
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <HeroSection />
+      <Specials />
+      <Footer />
+    </>
+  )
+}
+
+export default Home
